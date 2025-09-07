ad82b146438686c43896000872ce56652874acea
Mathieu762005 <mathieu762005@gmail.com>
2025-09-07 14:34:05 +0200

    maj

--- src/router.php
+++ src/router.php
@@ -1,8 +1,8 @@
 <?php
-include_once __DIR__ . '/controllers/HomeController.php';
-$controller = new HomeController();
-$controller->index();
-
 if (isset($_GET["page"]) && $_GET["page"] === "details") {
-    include "details.php";
+    include "views/details.php";
+} else {
+    include_once __DIR__ . '/controllers/HomeController.php';
+    $controller = new HomeController();
+    $controller->index();
 }
--- src/views/details.php
+++ src/views/details.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>détails</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
+
 <body>
     <main>
-        <h1>dddddd</h1>
+        <div>
+            <?php foreach ($allPokemons as $pokemon): ?>
+                <img src="<?= $pokemon["image"] ?>" alt="<?= $pokemon["name"] ?>">
+                <p class="card-text"><?= $pokemon["description"] ?></p>
+            <?php endforeach; ?>
+        </div>
     </main>
 </body>
+
 </html>
